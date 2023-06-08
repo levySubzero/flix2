e0cc35304b8a9a747c1134d75c3b4121d92b8c5e
levySubzero <ochieng.levy.vyle@gmail.com>
2023-06-08 22:06:54 +0300

    git rem

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -89,9 +89,9 @@ const Auth = () => {
               <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FcGoogle size={32} />
               </div>
-              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              {/* <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FaGithub size={32} />
-              </div>
+              </div> */}
             </div> 
             <p className="text-neutral-500 mt-12">
               {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
