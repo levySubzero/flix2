a9214fb0fac4f156bd5fd617a140d23889b7c2e0
levySubzero <ochieng.levy.vyle@gmail.com>
2023-06-08 21:15:22 +0300

    updtes

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -10,6 +10,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const { currentUser } = await serverAuth(req, res);
 
       const { movieId } = req.body;
+
+      console.log(movieId)
   
       const existingMovie = await prismadb.movie.findUnique({
         where: {
@@ -39,6 +41,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const { currentUser } = await serverAuth(req, res);
 
       const { movieId } = req.body;
+
+      console.log(movieId)
 
       const existingMovie = await prismadb.movie.findUnique({
         where: {
