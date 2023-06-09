da7ba39ffbe5d98b5a3a0f711d84f5dc0db1f490
levySubzero <ochieng.levy.vyle@gmail.com>
2023-06-09 17:19:42 +0300

    removed comments

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -8,9 +8,6 @@ import { authOptions } from "@/pages/api/auth/[...nextauth]";
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions);
 
-// const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
-//   const session = await getSession({ req });
-
   if (!session?.user?.email) {
     throw new Error('Not signed in');
   }
