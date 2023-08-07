9dcf95ae160665646f7b8c0f544b5151f3020fbf
levySubzero <ochieng.levy.vyle@gmail.com>
2023-08-07 04:58:55 +0300

    debug 2

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -12,12 +12,13 @@ const MovieList: React.FC<ItemInterface> = ({ movies, shows, title }) => {
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold ">{title}</p>
       </div>
       <div className="flex mt-2">
-        {shows.length > 0 && items.map((item, i) => (
-          <>
-            <MovieCard key={movies[i].id} data={movies[i]} />
-            <SeriesCard key={shows[i].id} data={shows[i]} />
-          </>
-        ))}
+      {shows.length > 0 && items.map((item, i) => (
+        <React.Fragment key={i}>
+          {movies[i] && <MovieCard key={`movie-${movies[i].id}`} data={movies[i]} />}
+          {shows[i] && <SeriesCard key={`series-${shows[i].id}`} data={shows[i]} />}
+        </React.Fragment>
+      ))}
+
         {shows.length === 0 && movies.map((movie) => (
           <>
             <MovieCard key={movie.id} data={movie} />
